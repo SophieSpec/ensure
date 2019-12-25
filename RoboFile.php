e6d44f7008cb2021e56ebe42f8ab210523cb96e9
Aurélien Delogu <dev@dreamysource.fr>
2019-12-25 08:12:31 +0100

    Checkout to master before creating tags

--- RoboFile.php
+++ RoboFile.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-class RoboFile extends Robo\Tasks
+final class RoboFile extends Robo\Tasks
 {
     public function publish(string $version): void
     {
+        $this->_exec('git checkout master');
         $this->_exec("git tag $version");
-        $this->_exec("git push");
-        $this->_exec("git push --tags");
+        $this->_exec('git push');
+        $this->_exec('git push --tags');
     }
 
     public function spec(): void
